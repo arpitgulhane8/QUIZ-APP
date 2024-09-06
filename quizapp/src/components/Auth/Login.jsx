cf3b4530a300d9ef8a65b10c69878271341cfa9b
arpitgulhane8 <162834636+arpitgulhane8@users.noreply.github.com>
2024-09-06 20:18:28 +0530

    Update Login.jsx

--- quizapp/src/components/Auth/Login.jsx
+++ quizapp/src/components/Auth/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
     e.preventDefault();
   
     try {
-      const response = await axios.post('/api/auth/login', {
+      const response = await axios.post('https://quiz-app-backend-nzng.onrender.com/api/auth/login', {
         email,
         password,
       });
